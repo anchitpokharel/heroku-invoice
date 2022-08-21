9889513a4a5e74cd96d9394817d8da6f9caee77b
Anchit Pokharel <pokharelanchit@gmail.com>
2022-08-21 16:34:46 +0545

    new procfile commands

--- invoice/settings.py
+++ invoice/settings.py
@@ -70,6 +70,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "invoice.urls"
